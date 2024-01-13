c8f95c8b0d68c6158be27879d6a52053365f65d8
Changhae Lee <chlee973@kaist.ac.kr>
2024-01-14 01:39:06 +0900

    DB sql 작성, db 입출력 테스트를 위한 form 작성

---
+++ src/lib/db.js
@@ -0,0 +1,12 @@
+import { Pool } from 'pg';
+
+// PostgreSQL 데이터베이스 연결 설정
+const pool = new Pool({
+	user: 'kalkal',
+	host: 'localhost',
+	database: 'kalkal_db',
+	password: 'sang@830139',
+	port: 5432 // 기본 포트 번호
+});
+
+export default pool;
---
+++ src/routes/quote/create/+page.svelte
@@ -0,0 +1,19 @@
+<form method="POST">
+	<label>
+		제목
+		<input name="title" type="text" />
+	</label>
+	<label>
+		닉네임
+		<input name="nickname" type="text" />
+	</label>
+	<label>
+		비밀번호
+		<input name="password" type="password" />
+	</label>
+	<label>
+		내용
+		<textarea name="content" />
+	</label>
+	<button type="submit">등록</button>
+</form>
