80975bdb255839de3d416f0e06a3b0b26d23d110
Changhae Lee <chlee973@kaist.ac.kr>
2024-01-19 11:31:52 +0900

    dotenv 설정

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,11 +3,11 @@ import pg from 'pg';
 const { Pool } = pg;
 // PostgreSQL 데이터베이스 연결 설정
 const pool = new Pool({
-	user: 'kalkal',
-	host: 'localhost',
-	database: 'kalkal_db',
-	password: 'sang@830139',
-	port: 5432 // 기본 포트 번호
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	database: process.env.DB_DB,
+	password: process.env.DB_PASSWORD,
+	port: Number(process.env.DB_PORT)
 });
 
 export default pool;
