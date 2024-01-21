b1ab83a311ee2505459ae8d13922af24693e1bb3
Changhae Lee <chlee973@kaist.ac.kr>
2024-01-21 15:24:27 +0900

    명언 게시판 비밀번호 bcrypt 암호화

--- src/routes/quote/create/+page.server.js
+++ src/routes/quote/create/+page.server.js
@@ -1,5 +1,6 @@
 import pool from '$lib/db';
 import { redirect } from '@sveltejs/kit';
+import bcrypt from 'bcrypt';
 /** @type {import('./$types').Actions} */
 export const actions = {
 	default: async ({ request }) => {
@@ -11,11 +12,18 @@ export const actions = {
 		const password = data.get('password');
 		const speaker = data.get('speaker');
 		const client = await pool.connect();
+
+		const saltRounds = 10;
+
 		let postId;
+		let hash = password;
+		if (typeof password === 'string') {
+			hash = await bcrypt.hash(password, saltRounds);
+		}
 		try {
 			const query = {
 				text: 'INSERT INTO quote.post (title, nickname, content, speaker, password) VALUES ($1, $2, $3, $4, $5) RETURNING id',
-				values: [title, nickname, content, speaker, password]
+				values: [title, nickname, content, speaker, hash]
 			};
 			const res = await client.query(query);
 			postId = res.rows[0].id;
