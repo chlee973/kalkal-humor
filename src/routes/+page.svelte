fee169fbff468a268fd0ff30ba0d8092e05d80b4
Changhae Lee <chlee973@kaist.ac.kr>
2024-01-27 11:40:57 +0900

    홈 화면 타이머 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,52 @@
-<h1>어서오세요</h1>
-<p>깔깔유머 사이트의 홈 화면이다.</p>
+<script>
+	import { browser } from '$app/environment';
+	import { onDestroy, onMount } from 'svelte';
+	let clock = '00:00:00';
+	let minutes = new Date().getMinutes();
+	let facts = '';
+	/** @type {number} */
+	let timer;
+	function digitalClock() {
+		const date = new Date();
+		const hours = date.getHours();
+		minutes = date.getMinutes();
+		const seconds = date.getSeconds();
+		/** @param {number} num*/
+		function zeroPad(num) {
+			return num < 10 ? '0' + num : '' + num;
+		}
+		clock = zeroPad(hours) + ':' + zeroPad(minutes) + ':' + zeroPad(seconds);
+		timer = setTimeout(digitalClock, 1000);
+	}
+	onMount(() => {
+		digitalClock();
+	});
+	onDestroy(() => {
+		if (timer) {
+			clearTimeout(timer);
+		}
+	});
+	$: {
+		if (browser) {
+			fetch(`http://numbersapi.com/${minutes}/math?json`)
+				.then((response) => response.json())
+				.then((data) => {
+					facts = data.text;
+				});
+		}
+	}
+</script>
+
+<h1>{clock}</h1>
+<p>{facts}</p>
+
+<style>
+	h1 {
+		font-size: 64px;
+		text-align: center;
+	}
+	p {
+		text-align: center;
+		font-style: italic;
+	}
+</style>
